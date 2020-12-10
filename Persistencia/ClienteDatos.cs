227751c7343dbd2cfa1cfc79169ad4ab09ac655c
Victor <vimartma@unirioja.loc>
2020-12-10 11:56:41 +0100

    PersCliente

---
+++ Persistencia/ClienteDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    class ClienteDatos
+    {
+        private string Dni;
+        private string Nombre;
+        private string Telefono;
+        private string Categoria;
+        private double Descuento;
+
+        protected ClienteDatos(string dni,string nombre,string telefono,string categoria,double descuento) {
+            this.Dni = dni;
+            this.Nombre = nombre;
+            this.Telefono = telefono;
+            this.Categoria = categoria;
+            this.Descuento = descuento;
+        }
+        public string DNI {
+            get {
+                return this.DNI;
+            }
+        }
+    }
+}
