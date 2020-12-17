a74b2ae15c77f21f6122215370d20b57b5ceee18
vimartma <vimartma@CCTL132-06>
2020-12-17 11:12:53 +0100

    ModDomCliente

---
+++ ModeloDominio/Cliente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    class Cliente
+    {
+        private string Dni;
+        private string nombre;
+        private string telefono;
+        private Categoria categoria;
+
+        public Cliente(string dni, string nom,string tlf,Categoria cat) {
+            this.Dni = dni;
+            this.nombre = nom;
+            this.telefono = tlf;
+            this.categoria = cat;
+        }
+        public string DNI
+        {
+            get
+            {
+                return this.DNI;
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+        }
+        public string Telefono
+        {
+            get
+            {
+                return this.telefono;
+            }
+            set {
+                this.telefono = value;
+                }
+        }
+        public Categoria Categoria
+        {
+            get
+            {
+                return this.categoria;
+            }
+            set
+            {
+                this.categoria = value;
+            }
+        }
+    }
+}
---
+++ Persistencia/PersistenciaCliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    class PersistenciaCliente
+    {
+        public static void AltaCliente(Cliente c) { 
+        
+        }
+    }
+}
