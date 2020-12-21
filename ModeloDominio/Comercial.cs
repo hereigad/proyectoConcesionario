32aafc5f4d2f58c1ed425492625454ac3dec204e
albanita <43749327+albanita@users.noreply.github.com>
2020-12-21 20:37:18 +0100

    Presupuesto hecho
    He implementado el diagrama de clases de presupuesto

---
+++ ModeloDominio/Comercial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    public class Comercial
+    {
+        private string codigo;
+        private string nombre;
+        private string apellido;
+
+        public Comercial(string codigo, string nombre, string apellido)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public string Codigo
+        {
+            get
+            {
+                return this.codigo;
+            }
+        }
+    }
+}
---
+++ ModeloDominio/Presupuesto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    public class Presupuesto
+    {
+        private DateTime fechaRealizacion;
+        private string id;
+
+        private EstadoPresupuesto estado;
+        private Vehiculo[] vehiculos;
+        private Cliente cliente;
+        private Comercial comercial;
+
+        public Presupuesto(string id, DateTime fecha, EstadoPresupuesto estado, Comercial comercial, Cliente cliente, Vehiculo[] vehiculos)
+        {
+            this.fechaRealizacion = fecha;
+            this.id = id;
+            this.comercial = comercial;
+            this.cliente = cliente;
+            this.vehiculos = vehiculos;
+            this.estado = estado;
+        }
+
+        public Cliente Cliente
+        {
+            get
+            {
+                return this.cliente;
+            }
+        }
+
+        public EstadoPresupuesto Estado
+        {
+            get
+            {
+                return this.estado;
+            }
+        }
+
+        public bool isAceptado()
+        {
+            return this.estado == EstadoPresupuesto.Aceptado;
+        }
+
+        public string ID
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+    }
+}
