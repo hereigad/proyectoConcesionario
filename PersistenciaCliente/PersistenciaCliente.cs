5115b9d646754531828d79760a80a3f4b4adbf9a
vimartma <55494039+vimartma@users.noreply.github.com>
2020-12-29 18:54:43 +0100

    p

--- PersistenciaCliente/PersistenciaCliente.cs
+++ PersistenciaCliente/PersistenciaCliente.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using ModeloDominio;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PersistenciaCliente
 {
     class PersistenciaCliente
     {
+        public void anadirCliente(Cliente c) { 
+
+            
+        }
+
     }
 }
