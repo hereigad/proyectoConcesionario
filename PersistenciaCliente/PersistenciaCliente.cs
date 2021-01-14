5177497665a89d1ab05261f5f98364b90fa00800
vimartma <vimartma@unirioja.loc>
2021-01-14 11:55:00 +0100

    Cambios_Persistencia_Cliente

--- PersistenciaCliente/PersistenciaCliente.cs
+++ PersistenciaCliente/PersistenciaCliente.cs
@@ -5,22 +5,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Persistencia;
 
 namespace PersistenciaCliente
 {
     class PersistenciaCliente
     {
 
-        public void anadirCliente(Cliente c) { 
-               
-            
+        public bool anadirCliente(Cliente c) {
+            BD.INSERT_Cliente(c);
+            if (BD.EXISTE_Cliente(c)) {
+                return true;
+            }
+            return false;
         }
         public Cliente seleccionarCliente(Cliente c) {
-            Cliente cli;
+            ClienteDatos cd = BD.SELECT_Cliente(c);
+            if (cd.CategoriaCliente == 5) {
+                return new Cliente(cd.DNI, cd.NombreCliente, cd.TlfCliente,Categoria.A);
+            }
+            if (cd.CategoriaCliente == 10) {
+                return new Cliente(cd.DNI, cd.NombreCliente, cd.TlfCliente, Categoria.B);
+            }
+            if (cd.CategoriaCliente ==15){
+                return new Cliente(cd.DNI, cd.NombreCliente, cd.TlfCliente, Categoria.C);
+            }
+
+            return null;
+        }
+
+        public bool eliminaCliente(Cliente c) {
+            BD.DELETE_Cliente(c);
+            if (BD.EXISTE_Cliente(c)) {
+                return false;
+            }
+            return true;  
+        }
+
+        public bool existeCliente(Cliente c) {
+            if (BD.EXISTE_Cliente(c)) {
+                return true;
+            }
+            return false;
+        }
+
+        public List<Presupuesto> presupuestosDeCliente(Cliente c) {
+            List<Presupuesto> lista = new List<Presupuesto>();
 
 
-            return cli;
+        
         }
 
     }
