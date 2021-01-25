8e88eadee6ccb2ae2d9db6c7ae02a4dba754014c
albanita <43749327+albanita@users.noreply.github.com>
2021-01-25 22:14:45 +0100

    ok

--- PresentacionPresupuesto/BusquedaPorCliente.Designer.cs
+++ PresentacionPresupuesto/BusquedaPorCliente.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listPresupuestos = new System.Windows.Forms.ListBox();
+            this.vistaPresupuesto1 = new VistaPresupuesto();
+            this.btnCerrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listPresupuestos
+            // 
+            this.listPresupuestos.FormattingEnabled = true;
+            this.listPresupuestos.Location = new System.Drawing.Point(12, 12);
+            this.listPresupuestos.Name = "listPresupuestos";
+            this.listPresupuestos.Size = new System.Drawing.Size(222, 342);
+            this.listPresupuestos.TabIndex = 0;
+            this.listPresupuestos.SelectedIndexChanged += new System.EventHandler(this.listPresupuestos_SelectedIndexChanged);
+            // 
+            // vistaPresupuesto1
+            // 
+            this.vistaPresupuesto1.Location = new System.Drawing.Point(240, 12);
+            this.vistaPresupuesto1.Name = "vistaPresupuesto1";
+            this.vistaPresupuesto1.Size = new System.Drawing.Size(327, 294);
+            this.vistaPresupuesto1.TabIndex = 1;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(492, 331);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.button1_Click);
             // 
             // BusquedaPorCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 493);
+            this.ClientSize = new System.Drawing.Size(576, 363);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.vistaPresupuesto1);
+            this.Controls.Add(this.listPresupuestos);
             this.Name = "BusquedaPorCliente";
             this.Text = "BusquedaCliente";
             this.ResumeLayout(false);
@@ -42,5 +74,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listPresupuestos;
+        private VistaPresupuesto vistaPresupuesto1;
+        private System.Windows.Forms.Button btnCerrar;
     }
 }
--- PresentacionPresupuesto/BusquedaPorCliente.cs
+++ PresentacionPresupuesto/BusquedaPorCliente.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ModeloDominio;
+using PresentacionPresupuesto;
 
 namespace PresentacionPresupuesto
 {
@@ -27,12 +28,28 @@ namespace PresentacionPresupuesto
             this.logicaP = lp;
             this.logicaC = lc;
             this.dni = dni;
+            this.devolverPresupuestos();
         }
 
         private void devolverPresupuestos()
         {
-            Cliente cli = this.logicaC.selCliente(new Cliente(this.dni, "", "", Categoria.A));
-            this.presupuestos = this.logicaP.obtenerPresupuestosCliente(cli);
+            //Cliente cli = this.logicaC.selCliente(new Cliente(this.dni, "", "", Categoria.A));
+            this.presupuestos = this.logicaP.obtenerPresupuestosCliente(new Cliente(this.dni, "", "", Categoria.A));
+            foreach(Presupuesto p in this.presupuestos)
+            {
+                this.listPresupuestos.Items.Add(p.ID);
+            }
+        }
+
+        private void listPresupuestos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Presupuesto p = this.presupuestos.ElementAt(this.listPresupuestos.SelectedIndex);
+            this.vistaPresupuesto1 = new VistaPresupuesto(p);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
