11bb330b99b0946c87ae3827d7ca534c9abc96ed
albanita <43749327+albanita@users.noreply.github.com>
2021-01-26 13:32:57 +0100

    ok

--- PresentacionPresupuesto/BusquedaPorCliente.Designer.cs
+++ PresentacionPresupuesto/BusquedaPorCliente.Designer.cs
@@ -31,6 +31,7 @@
             this.vistaPresupuesto = new PresentacionPresupuesto.VistaPresupuesto();
             this.btnCerrar = new System.Windows.Forms.Button();
             this.listPresupuestos = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // vistaPresupuesto
@@ -53,22 +54,34 @@
             // listPresupuestos
             // 
             this.listPresupuestos.FormattingEnabled = true;
-            this.listPresupuestos.Location = new System.Drawing.Point(12, 12);
+            this.listPresupuestos.Location = new System.Drawing.Point(12, 28);
             this.listPresupuestos.Name = "listPresupuestos";
-            this.listPresupuestos.Size = new System.Drawing.Size(291, 329);
+            this.listPresupuestos.Size = new System.Drawing.Size(291, 316);
             this.listPresupuestos.TabIndex = 2;
+            this.listPresupuestos.SelectedIndexChanged += new System.EventHandler(this.listPresupuestos_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Presupuestos:";
             // 
             // BusquedaPorCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(621, 389);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listPresupuestos);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.vistaPresupuesto);
             this.Name = "BusquedaPorCliente";
             this.Text = "BusquedaPorCliente";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,5 +90,6 @@
         private VistaPresupuesto vistaPresupuesto;
         private System.Windows.Forms.Button btnCerrar;
         private System.Windows.Forms.ListBox listPresupuestos;
+        private System.Windows.Forms.Label label1;
     }
 }
--- PresentacionPresupuesto/BusquedaPorCliente.cs
+++ PresentacionPresupuesto/BusquedaPorCliente.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ModeloDominio;
 
 namespace PresentacionPresupuesto
 {
@@ -14,6 +15,7 @@ namespace PresentacionPresupuesto
     {
         private LogicaNegocioPresupuesto.LogicaPresupuesto lnp;
         private string dni;
+        private List<Presupuesto> presupuestos;
         public BusquedaPorCliente()
         {
             InitializeComponent();
@@ -23,11 +25,31 @@ namespace PresentacionPresupuesto
         {
             this.dni = d;
             this.lnp = lp;
+            this.rellenarDatos();
+        }
+
+        /// <summary>
+        /// pre: -
+        /// post: rellena la lista de presupuestos con el identificador de los presupuestos del cliente
+        /// </summary>
+        private void rellenarDatos()
+        {
+            this.presupuestos = this.lnp.obtenerPresupuestosCliente(new Cliente(this.dni, "", "", Categoria.A));
+            foreach(Presupuesto p in this.presupuestos)
+            {
+                this.listPresupuestos.Items.Add(p.ID);
+            }
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void listPresupuestos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Presupuesto p = this.presupuestos.ElementAt(this.listPresupuestos.SelectedIndex);
+            this.vistaPresupuesto = new VistaPresupuesto(p);
         }
     }
 }
